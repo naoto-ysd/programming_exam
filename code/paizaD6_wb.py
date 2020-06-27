046707ece939e7ce0789df04ca18add8a698f86b
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-06-28 06:06:12 +0900

    Craate paizaD6_wb.py

---
+++ code/paizaD6_wb.py
@@ -0,0 +1,13 @@
+num = int(input())
+i = 1
+
+while i <= num:
+    if i % 15 == 0:
+        print("Fizz Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i += 1
