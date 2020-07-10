dd064b49abd9e0846dc20ee03c77d8ebbf8701f7
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-07-11 05:19:33 +0900

    Create paizaD27.py and paizaD41.py

---
+++ code/paizaD27.py
@@ -0,0 +1,6 @@
+n = int(input())
+count = 0
+
+for i in range(n):
+    count += i + 1
+print(count)
---
+++ code/paizaD41.py
@@ -0,0 +1,9 @@
+n,d,e = input().split()
+n = int(n)
+d = int(d)
+e = int(e)
+
+if d*e <= n:
+    print("NG")
+else:
+    print("OK")
