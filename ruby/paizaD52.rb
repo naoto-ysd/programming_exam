743f33c1538d2e7345fd4c0122ec8ddd4af0f619
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-07-17 00:03:04 +0900

    Create paizaD52.rb

---
+++ ruby/paizaD52.rb
@@ -0,0 +1,16 @@
+# N = int(input())
+# piramid = 0
+
+# for i in range(N+1):
+#     piramid = piramid + i
+
+# print(piramid)
+
+N = gets.to_i
+p = 0
+
+for i in 1..N do
+  p = p + i
+end
+
+print p
