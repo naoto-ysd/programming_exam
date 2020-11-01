c181456862a35faca96313a71343ffb2afe529b3
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-01 23:36:51 +0900

    Create paizaB21

---
+++ python/paizaB21.py
@@ -0,0 +1,36 @@
+N = int(input())
+
+arr = []
+fukusu = ""
+
+
+for n in range(N):
+  # 単語の入力
+  tango = input()
+
+  # 単語に複数形が含まれるか検索
+  # ichi = len(tango) - tango.find("s")
+  ichi = tango.find("s")
+
+  print(ichi)
+
+  # 単語末尾の1文字をとる
+  # tango1 =
+  # 単語の末尾の2文字をとる
+  # tango2 = 
+
+  
+
+
+
+  # if :
+    
+  # else:
+    
+
+  # 配列に複数形に変換した単語を入れる
+  arr.append(tango)
+
+# 配列の出力
+for item in arr:
+  print(item)
