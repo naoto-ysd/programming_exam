ec93790cc63e92dc0a34f977eb07effd6dfb7914
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-02 01:25:34 +0900

    Fix C985

--- python/paizaC85.py
+++ python/paizaC85.py
@@ -1,16 +1,18 @@
+keybord = input()
+taikyu = keybord.split()
+taikyu = [int(t) for t in taikyu]
+
 S = input()
-len = len(S)
 
-for n in range(3):
-  plus = ""
 
-  i = 1
-  while i <= len+2:
-    if n == 1:
-      plus = "+" + S + "+" 
-    else:
-      plus = plus + "+"
-    
-    i += 1
+alfa = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-  print(plus)
+answer = ""
+
+for n in S:
+  if taikyu[alfa.index(n)] >= 1:
+    answer = answer + n
+  
+  taikyu[alfa.index(n)] = taikyu[alfa.index(n)]-1
+
+print(answer)
