4460ddf250d71c7506ced4565562e3129a22a104
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-02 00:23:16 +0900

    Fix B21

--- python/paizaB21.py
+++ python/paizaB21.py
@@ -14,16 +14,24 @@ for n in range(N):
   elif len(tango) - tango.rfind("f") == 1:
     # 単語末尾のfをとる
     tango = tango.rstrip("f") + "ves"
-    tango
 
   elif len(tango) - tango.rfind("fe") == 2:
     # 単語の末尾のfeをとる
     tango = tango.rstrip("fe") + "ves"
+  
+  elif len(tango) - tango.rfind("y") == 1:
+    matsubi2 = tango[len(tango)-2:len(tango)-1]
+    if matsubi2 != "a" and matsubi2 != "i" and matsubi2 != "u" and matsubi2 != "e" and matsubi2 != "o":
+      tango = tango[0:len(tango)-1] + "ies"
+    else:
+      tango = tango + "s"
 
-    
+  else:
+    tango = tango + "s"
 
   # 配列に複数形に変換した単語を入れる
   arr.append(tango)
+  
 
 # 配列の出力
 for item in arr:
