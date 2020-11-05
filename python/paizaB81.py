99701ba1d63f464804cbeb01b4203a2e2601e325
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-05 23:59:52 +0900

    update paizab21

--- python/paizaB81.py
+++ python/paizaB81.py
@@ -7,6 +7,22 @@ for n in range(int(H)):
     kadan.append(input())
 
 # 配列の繰り返し処理
-for i in range(int(H)):
-    # 文字列に＃が含まれていた場合
-    if kadan[i].find("#") >= 0:
+for n in range(int(H)):
+    for i in range(int(W)):
+        # 文字列に＃が含まれていた場合
+        if kadan[n][i] == "#":
+            himo += 4
+            # 上のマスの確認
+            if kadan[n-1][i] == "#":
+                himo =  himo -1
+            # 下のマスの確認
+            elif kadan[n+1][i] == "#":
+                himo =  himo -1
+            # 右のマスの確認
+            elif kadan[n][i+1] == "#":
+                himo =  himo -1
+            # 左のマスの確認
+            elif kadan[n][i-1] == "#":
+                himo =  himo -1
+
+print(himo)
