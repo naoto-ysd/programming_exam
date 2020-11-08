81557b8a42d35a03541d43f9faadc84961ee7358
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-08 23:28:58 +0900

    fix

--- python/paizaC_runkup.py
+++ python/paizaC_runkup.py
@@ -1,5 +1,28 @@
 n = int(input())
 
-for i range(n):
+arr = []
+
+for i in range(n):
     a,b = input().split()
-    a ,
+    if a == b:
+        arr.append(int(a) * int(b))
+    else:
+        arr.append(int(a) + int(b))
+
+print(sum(arr))
+
+
+# 回答例
+# time = int(input())
+# result = 0
+
+# for i in range(time):
+#     std_in = input()
+#     array = std_in.split()
+
+#     if array[0] == array[1]:
+#         result += int(array[0]) * int(array[1])
+#     else:
+#         result += int(array[0]) + int(array[1])
+
+# print(result)
