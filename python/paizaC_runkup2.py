837f0b14fb76d99c7453fca11d3a94f28ef2215f
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-08 23:30:20 +0900

    create

---
+++ python/paizaC_runkup2.py
@@ -0,0 +1,13 @@
+time = int(input())
+result = 0
+
+for i in range(time):
+    std_in = input()
+    array = std_in.split()
+
+    if array[0] == array[1]:
+        result += int(array[0]) * int(array[1])
+    else:
+        result += int(array[0]) + int(array[1])
+
+print(result)
