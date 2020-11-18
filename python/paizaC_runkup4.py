3efd59e74a9a347e76d03a930a17e67757d8103c
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-18 23:13:49 +0900

    fix

---
+++ python/paizaC_runkup4.py
@@ -0,0 +1,28 @@
+s = input()
+t = input()
+count = 0
+
+s_length = len(s)
+t_length = len(t)
+
+for n in range(t_length):
+    if s == t[n:n+s_length]:
+        count += 1
+
+print(count)
+
+ll
+HelloPaiza
+
+# 模範回答
+# pattern = input()
+# string = input()
+# result = 0
+
+# for i in range(len(string) - len(pattern) + 1):
+#     portion = string[i:i + len(pattern)]
+
+#     if portion == pattern:
+#         result += 1
+
+# print(result)
