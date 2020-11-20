fa3c2515aa4aa28a472fd9ef79320d798bb06c53
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-11-20 23:59:40 +0900

    finish

--- python/paizaC_runkup5.py
+++ python/paizaC_runkup5.py
@@ -3,9 +3,10 @@ inputs = {}
 
 for i in range(num):
     tmp = input().split()
+    # 入力された数字をキーにして、キーに対応した文字列を設定している
     inputs[int(tmp[1])] = tmp[0]
-    print(inputs)
 
+print(inputs.items())
 inputs = sorted(inputs.items())
 
 # for i in inputs:
