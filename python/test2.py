4dccd8bc819c3f73ea671ba16dbe09f933085f6a
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-12-20 23:45:50 +0900

    fix

--- python/test2.py
+++ python/test2.py
@@ -1,24 +1,11 @@
-# 使用言語 Python
-class Login:
-    def login_id(UserID,UserName,LoginID,Password):
-        user_id = UserID
-        user_name = UserName
-        login_id = LoginID
-        password = Password
-    
-    def auth(login_id, password):
+class Spam:
+    val = 100
+    def ham(self):
+        self.egg('call method')
 
-        for num in range(3):
-            if login_id == user.login_id and password == user.password:
-                # JSONで出力
-            
-            # 認証に3回以上失敗した場合
-            elif num >= 2:
-                # 一時的にログインできないようにする
+    def egg(self,msg):
+        print("{0}".format(msg))
+        print(("{0}".format(self.val)))
 
-
-# オブジェクトの作成
-request = Login.new(UserID,UserName,LoginID,Password):
-
-# ユーザー認証
-auth(request.login_id, request.password)
+spam = Spam()
+spam.ham()
