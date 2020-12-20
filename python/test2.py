4440f2e90af791d8b56cdeb74e9e5e27bc1dddbd
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-12-20 23:49:23 +0900

    fix

--- python/test2.py
+++ python/test2.py
@@ -21,3 +21,21 @@ class Spam:
 
 spam = Spam(5,10)
 spam.output()
+
+# 継承
+class Base:
+    basevalue = "base"
+    def spam(self):
+        print("Base.spam()")
+
+    def ham(self):
+        print("ham")
+
+class Derived(Base):
+    def spam(self):
+        print ("Derived.spam()")
+        self.ham()
+
+derived = Derived()
+print("{0}".format(derived.basevalue))
+derived.ham()
