eebeee1611d29aa2d660cd5fa6d2ceae20b6e354
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-12-20 23:48:32 +0900

    fix

--- python/test2.py
+++ python/test2.py
@@ -9,3 +9,15 @@ class Spam:
 
 spam = Spam()
 spam.ham()
+
+
+class Spam:
+    def __init__(self,ham,egg):
+        self.ham = ham
+        self.egg = egg
+    def output(self):
+        sum = self.ham + self.egg
+        print("{0}".format(sum))
+
+spam = Spam(5,10)
+spam.output()
