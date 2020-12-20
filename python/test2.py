5a0206779b726f71cb07306abece7d1117972536
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-12-20 23:43:25 +0900

    create

---
+++ python/test2.py
@@ -0,0 +1,24 @@
+# 使用言語 Python
+class Login:
+    def login_id(UserID,UserName,LoginID,Password):
+        user_id = UserID
+        user_name = UserName
+        login_id = LoginID
+        password = Password
+    
+    def auth(login_id, password):
+
+        for num in range(3):
+            if login_id == user.login_id and password == user.password:
+                # JSONで出力
+            
+            # 認証に3回以上失敗した場合
+            elif num >= 2:
+                # 一時的にログインできないようにする
+
+
+# オブジェクトの作成
+request = Login.new(UserID,UserName,LoginID,Password):
+
+# ユーザー認証
+auth(request.login_id, request.password)
