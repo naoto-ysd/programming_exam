2fa8deed9575fee70f93c2863adcc34a82cbeae8
naoto-ysd <61533915+naoto-ysd@users.noreply.github.com>
2021-10-06 02:53:11 +0900

    ブログ用

---
+++ ruby/hatena_day4.rb
@@ -0,0 +1,8 @@
+p 'Please enter a number'
+
+# The following is input
+# In this process, the input value substitute to the variable "i".
+i = gets.to_i
+
+# In this following process, the input value multipled by 2 and output.
+p 'The input value multipled by 2 is ' + (i*2).to_s
