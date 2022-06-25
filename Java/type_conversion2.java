3c94c8353164def3b1b8ffd9769c42d36dd2332c
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2022-06-25 14:45:57 +0900

    create program about type conversion from integer to string

--- Java/type_conversion2.java
+++ Java/type_conversion2.java
@@ -1,4 +1,4 @@
-publlc class Main{
+public class Main{
   public static void main(String[] args){
     double d = 8.5 / 2;
     long l = 5 + 2L;
---
+++ Java/type_conversion3.java
@@ -0,0 +1,7 @@
+public class Main{
+  public static void main(String[] args){
+    // Execution this code results in an error.
+    String msg = "私の年齢は + 23";
+    System.out.println(msg);
+  }
+}
