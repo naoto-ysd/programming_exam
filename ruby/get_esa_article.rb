6b6f0b30c6613acd1a5baec14e2e5dc293025149
Naoto Yoshida <naoto346@gmail.com>
2023-01-27 00:18:16 +0900

    get posts

--- ruby/get_esa_article.rb
+++ ruby/get_esa_article.rb
@@ -1,9 +1,15 @@
+require 'esa'
 require 'uri'
 require 'net/http'
-client = Esa::Client.new(access_token: ENV['GET_ARTICLES'], current_team: ENV['ESA_TEAM'])
-p client.user
-# /v1/teams/docs/posts
+require 'json'
 
-# uri = URI('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-# res = Net::HTTP.get_response(uri)
-# puts res.body if res.is_a?(Net::HTTPSuccess)
+client = Esa::Client.new(access_token: ENV['GET_ARTICLES'], current_team: ENV['ESA_TEAM'])
+res = client.posts
+
+uri = URI("https://api.esa.io/v1/teams/docs/posts?access_token=#{ENV['GET_ARTICLES']}")
+res = Net::HTTP.get_response(uri)
+
+File.open("posts.txt", mode = "w"){|f|
+  f.write(res.body)  # ファイルに書き込む
+}
+
