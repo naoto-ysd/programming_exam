4daaf88bacdac41f9f2b54219eb0902533549957
Naoto Yoshida <naoto346@gmail.com>
2023-01-26 23:20:23 +0900

    gem install

--- ruby/get_esa_article.rb
+++ ruby/get_esa_article.rb
@@ -1,10 +1,9 @@
 require 'uri'
 require 'net/http'
-# puts ENV['GET_ARTICLES']
-# client = Esa::Client.new(access_token: ENV['GET_ARTICLES'], current_team: ENV['ESA_TEAM'])
-
+client = Esa::Client.new(access_token: ENV['GET_ARTICLES'], current_team: ENV['ESA_TEAM'])
+p client.user
 # /v1/teams/docs/posts
 
-uri = URI('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-res = Net::HTTP.get_response(uri)
-puts res.body if res.is_a?(Net::HTTPSuccess)
+# uri = URI('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+# res = Net::HTTP.get_response(uri)
+# puts res.body if res.is_a?(Net::HTTPSuccess)
