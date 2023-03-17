4943eba2cceafd08c803e631b26f34aec997b3d5
Naoto Yoshida <naoto346@gmail.com>
2023-03-17 09:34:10 +0900

    fix grep_esa_link_method

--- ruby/grep_esalink.rb
+++ ruby/grep_esalink.rb
@@ -1,10 +1,12 @@
 # usage
 # ruby grep.rb hoge fuga
+require 'csv'
+
 def grep_esa_link(path,result_path)
-  File.open(result_path,'w') do |result_csv|
+  CSV.open(result_path,'w') do |result_csv|
     Dir.glob(File.join(path, '**/*.md')) do |filename|
       result = `grep -n "grep_word" "#{filename}"`
-      result_csv.write("#{filename}: #{result}") unless result.empty?
+      result_csv << ["#{filename}, #{result}"] unless result.empty?
     end
   end
 end
